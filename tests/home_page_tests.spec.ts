a4e1c6a8b4924187b5bbeff8d14b7f0cd7cc60e2
PK <grylion02@gmail.com>
2026-05-06 17:12:35 +0200

    home page tests

--- tests/home_page_tests.spec.ts
+++ tests/home_page_tests.spec.ts
@@ -1,13 +1,54 @@
 import { test, expect } from '@playwright/test';
+import { NavbarForNotLoggedIn } from '../page-objects/NavbarForNotLoggedIn';
+import { HomePageContent } from '../page-objects/HomePageContent';
+
+test.describe('Home page tests for not logged in user', () => {
+  let navBarForNotLoggedIn: NavbarForNotLoggedIn;
+  let homePageContent: HomePageContent;
+
+  test.beforeEach('go to page', async ({page}) => {
+    //Given
+    navBarForNotLoggedIn = new NavbarForNotLoggedIn(page);
+    homePageContent = new HomePageContent(page);
 
 
-test.beforeEach('go to page', async ({page}) => {
-  await page.goto('https://realworld.app.is')
-})
+    await page.goto('/')
+  })
 
 test('has title', async ({ page }) => {
-  //await page.goto('https://realworld.app.is/');
-
   // Expect a title "to contain" a substring.
   await expect(page).toHaveTitle(/Conduit/);
+});
+
+test('check elements on home page when user is not logged in', async ({ page }) => {
+  //Then
+  await expect(navBarForNotLoggedIn.homeLink).toBeVisible();
+  await expect(navBarForNotLoggedIn.logoLink).toBeVisible();
+  await expect(navBarForNotLoggedIn.signInLink).toBeVisible();
+  await expect(navBarForNotLoggedIn.signUpLink).toBeVisible();
+  await expect(homePageContent.globalFeedTab).toBeVisible();
+  await expect(homePageContent.yourFeedTab).toBeVisible();
+  await expect(homePageContent.headerOneConduit).toBeVisible();
+  await expect(homePageContent.paragraphAPlaceToShareYourKnowledge).toBeVisible();
+})
+
+test('navigation to Sign Up page and back works correctly', async ({ page }) => {
+  //When
+  await navBarForNotLoggedIn.signUpLink.click();
+  await expect(page.getByRole('heading', { level: 1, name: 'Sign up' })).toBeVisible(); //TO DO add this to page object later
+  await navBarForNotLoggedIn.homeLink.click();
+  
+  //Then
+  await expect(homePageContent.paragraphAPlaceToShareYourKnowledge).toBeVisible();
+});
+
+test('navigation to Sign in page and back works correctly', async ({ page }) => {
+  //When
+  await navBarForNotLoggedIn.signInLink.click();
+  await expect(page.getByRole('heading', { level: 1, name: 'Sign in' })).toBeVisible(); //TO DO add this to page object later
+  await navBarForNotLoggedIn.logoLink.click();
+  
+  //Then
+  await expect(homePageContent.paragraphAPlaceToShareYourKnowledge).toBeVisible();
+})
 });
