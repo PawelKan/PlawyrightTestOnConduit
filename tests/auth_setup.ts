0365725736cc66fbad7381ea05b977063c888a27
PK <grylion02@gmail.com>
2026-06-09 11:10:36 +0200

    changes in tests

--- tests/auth_setup.ts
+++ tests/auth_setup.ts
@@ -1,15 +1,49 @@
-import { test as setup } from './fixtures/fixtures';
+import { request, expect } from "@playwright/test";
+import { test as setup } from "./fixtures/fixtures";
+import { faker } from '@faker-js/faker'
 
-const pathToFile = 'tests/.auth/userLogged.json'
-setup ("Logged in user", async ({onLoginPage, onHomePage, page}) => {
-        
-        const userEmail: string = "myEmail@testtest.com"
-        const userPass: string = "myPass"
-        const userName: string = "myUsername"
+const pathToFile = "tests/.auth/RegisteredUserAuth.json";
 
-        await onLoginPage.openPage()
-        await onLoginPage.loginAsUser(userEmail, userPass);
-        await onHomePage.header.verifyUserIsLoggedIn(userName);
+setup("Register user with automatic login into app", async ({ page }) => {
+  //register and login have same cookies/local storage
+  await page.goto("/");
+  //get csrf token from html attribute
+  const csrf_token = await page
+    .locator('head meta[name="csrf-token"]')
+    .getAttribute("content");
 
-        await page.context().storageState({ path: pathToFile });
-})
+  if (!csrf_token) throw new Error("CSRF token not found");
+
+  // each time create new user
+  const userName = `testUser${faker.string.alphanumeric({ length: { min: 5, max: 10 } })}${faker.number.int({ min: 11, max: 1000 })}`;
+  const userMail = `${userName}@test.xyz`;
+  const userPass = "testPass123!";
+
+  const requestLoginUser = await page.request.post("/register", {
+    form: {
+      username: userName,
+      email: userMail,
+      password: userPass,
+      csrf_token: csrf_token,
+    },
+    headers: {
+      "X-CSRF-Token": csrf_token,
+      Referer: "https://realworld.app.is/login", // Dobra praktyka przy CSRF
+      "X-Requested-With": "XMLHttpRequest",
+    },
+  });
+
+  //check if new user was created
+  const status = requestLoginUser.status();
+  if (status === 200) {
+    await page.waitForURL("**/");
+    await page.context().storageState({ path: pathToFile });
+    process.env.USER_NAME = userName.toString();
+    process.env.USER_MAIL = userMail;
+    process.env.USER_PASS = userPass;
+  } else {
+    console.log(
+      `[Setup] Użytkownik nie został zarejestrowany (Status: ${status}). Pomijam aktualizację danych.`,
+    );
+  }
+});
--- tests/tests_register_and_login/ZZ_playground.spec.ts
+++ tests/tests_register_and_login/ZZ_playground.spec.ts
@@ -1,6 +1,6 @@
 import { test, expect } from '@playwright/test';
 
-test.skip("request api - get", async ({page, request}) => {
+test.skip("request api - get", async ({ page, request }) => {
   const requestHomePage = await request.get('/')
   const requestText = await requestHomePage.text()  //receive html text from request
   //console.log(requestText)
@@ -8,7 +8,7 @@ test.skip("request api - get", async ({page, request}) => {
   await page.goto('/')
 })
 
-test.skip("request api - login user with csrf token", async ({page, request}) => {
+test.skip("request api - login user with csrf token", async ({ page, request }) => {
   await page.goto('/')
   const csrf_token = await page.locator('head meta[name="csrf-token"]').getAttribute("content")
   if (!csrf_token) throw new Error('CSRF token not found')
@@ -21,13 +21,50 @@ test.skip("request api - login user with csrf token", async ({page, request}) =>
       password: userPass,
       csrf_token: csrf_token
     },
-  headers: {
+    headers: {
       'X-CSRF-Token': csrf_token,
       'Referer': 'https://realworld.app.is/login', // Dobra praktyka przy CSRF
       'X-Requested-With': 'XMLHttpRequest'
-    }    
+    }
   })
-  
+
   await page.goto('/')
   await page.reload()
 })
+test.skip("request api - register user with csrf token", async ({ page, request }) => {
+  await page.goto('/')
+  const csrf_token = await page.locator('head meta[name="csrf-token"]').getAttribute("content")
+  if (!csrf_token) throw new Error('CSRF token not found')
+  const usernName = 'testUser'
+  const userMail = "testUsername@mailtest1231.com"
+  const userPass = "testUsername123"
+
+  const requestLoginUser = await page.request.post('/register', {
+    form: {
+      username: usernName,
+      email: userMail,
+      password: userPass,
+      csrf_token: csrf_token
+    },
+    headers: {
+      'X-CSRF-Token': csrf_token,
+      'Referer': 'https://realworld.app.is/login', // Dobra praktyka przy CSRF
+      'X-Requested-With': 'XMLHttpRequest'
+    }
+  })
+
+  await page.goto('/')
+  await page.reload()
+})
+
+// setup ("Login user with UI", async ({onLoginPage, onHomePage, page}) => {
+//         const userEmail: string = "myEmail@testtest.com"
+//         const userPass: string = "myPass"
+//         const userName: string = "myUsername"
+
+//         await onLoginPage.openPage()
+//         await onLoginPage.loginAsUser(userEmail, userPass);
+//         await onHomePage.header.verifyUserIsLoggedIn(userName);
+
+//         await page.context().storageState({ path: pathToFile });
+// })
