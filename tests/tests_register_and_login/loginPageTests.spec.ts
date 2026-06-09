bd0575826a819af99b0a9a30014cf09d7b563ef6
PK <grylion02@gmail.com>
2026-06-09 15:00:51 +0200

    add negative tests for login page

--- tests/tests_register_and_login/loginPageTests.spec.ts
+++ tests/tests_register_and_login/loginPageTests.spec.ts
@@ -1,25 +1,33 @@
 import { test, expect } from '../fixtures/fixtures';
 
-test.describe('Login page tests', () =>{
-    
-    test.use({ storageState: { cookies: [], origins: [] } }); // for not logged in tests
-    
-    test.beforeEach('go to Login page', async ({onLoginPage}) => {
-        //Given
-        await onLoginPage.openPage();
-    });
 
-    test('verify elements on login page', async ({ onLoginPage}) => {
+test.use({ storageState: { cookies: [], origins: [] } }); // for not logged in tests
+
+test.beforeEach('open Login page', async ({ onLoginPage }) => {
+    await onLoginPage.openPage();
+});
+
+test.describe('Positive Login page tests', () => {
+
+    test('verify elements on login page', async ({ onLoginPage }) => {
         await onLoginPage.verifyElements();
     });
 
-    test('login with valid credentials', async({onHomePage, onLoginPage}) => {
-        // const userEmail: string = "myEmail@testtest.com"
-        // const userPass: string = "myPass"
-        // const userName: string = "myUsername"
-        
-
-        await onLoginPage.loginAsUser(process.env.USER_MAIL, process.env.USER_PASS);
+    test('login with valid credentials', async ({ onHomePage, onLoginPage }) => {
+        await onLoginPage.loginAsUser(process.env.USER_MAIL!, process.env.USER_PASS!);
         await onHomePage.header.verifyUserIsLoggedIn(process.env.USER_NAME!);
+    })
+})
+
+test.describe('Negative Login page tests', () => {
+
+    test('should not login with invalid email', async ({ onLoginPage }) => {
+        await onLoginPage.loginAsUser('invalidTestMail@emailTest.com', process.env.USER_PASS!);
+        await expect(onLoginPage.page.getByText("Invalid email or password")).toBeVisible()
+    })
+
+    test('should not login with invalid password', async ({ onLoginPage }) => {
+        await onLoginPage.loginAsUser(process.env.USER_MAIL!, "InvalidPassword111");
+        await expect(onLoginPage.page.getByText("Invalid email or password")).toBeVisible()
     })
 })
